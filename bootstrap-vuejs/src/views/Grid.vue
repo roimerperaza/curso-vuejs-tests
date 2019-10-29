0f8f49de18969112bb27565f56da02262c85473f
rperaza <roimerj63@gmail.com>
2019-10-29 09:00:17 -0500

    GRID VUE

--- bootstrap-vuejs/src/views/Grid.vue
+++ bootstrap-vuejs/src/views/Grid.vue
@@ -2,9 +2,30 @@
   <div class="mt-5">
     <b-container class="bv-example-row">
       <b-row>
-        <b-col cols="12" md="6" lg="4">1 of 3</b-col>
-        <b-col cols="12" md="6" lg="4">2 of 3</b-col>
-        <b-col cols="12" md="6" lg="4">3 of 3</b-col>
+        <b-col cols="12" md="6" lg="4">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo
+            necessitatibus maiores dolorum est praesentium pariatur sint
+            suscipit. Enim quidem esse, perspiciatis corporis, eum laborum
+            sequi, molestias officia quaerat exercitationem adipisci.
+          </p>
+        </b-col>
+        <b-col cols="12" md="6" lg="4">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. In dolores
+            minima iusto quae reiciendis, voluptate at similique sequi, dicta
+            ipsam delectus aut? Libero quae velit placeat suscipit distinctio
+            similique ad.
+          </p>
+        </b-col>
+        <b-col cols="12" md="6" lg="4">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor
+            voluptates praesentium hic provident, ipsam iusto earum sapiente
+            similique quibusdam reiciendis quam a obcaecati tempora minus fugit
+            vel itaque consequatur totam.
+          </p>
+        </b-col>
       </b-row>
     </b-container>
   </div>
