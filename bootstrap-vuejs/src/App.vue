dca44d92990af30bad2101b2c8f79fa8fb2fe54e
rperaza <roimerj63@gmail.com>
2019-10-28 22:46:01 -0500

    bootstrap-vuejs

---
+++ bootstrap-vuejs/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <!-- NAVBAR -->
+    <b-navbar toggleable="md" type="dark" variant="info">
+      <!-- TITLE NAVBAR -->
+      <b-navbar-brand :to="{ name: 'home' }" class="">NavBar</b-navbar-brand>
+      <!-- TOGGLE -->
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <!-- ITEMS NAVBAR -->
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item :to="{ name: 'home' }">Home</b-nav-item>
+          <b-nav-item :to="{ name: 'about' }">About</b-nav-item>
+          <b-nav-item :to="{ name: 'grid' }">Grid</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+
+    <!-- ROUTER VIEW -->
+    <b-container>
+      <router-view />
+    </b-container>
+  </div>
+</template>
---
+++ bootstrap-vuejs/src/views/Grid.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="mt-5">
+    <b-container class="bv-example-row">
+      <b-row>
+        <b-col cols="12" md="6" lg="4">1 of 3</b-col>
+        <b-col cols="12" md="6" lg="4">2 of 3</b-col>
+        <b-col cols="12" md="6" lg="4">3 of 3</b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Grid"
+};
+</script>
