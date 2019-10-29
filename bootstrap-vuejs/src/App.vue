6d84f6acb0408cfbc59390bc6ee9f71999d67be9
rperaza <roimerj63@gmail.com>
2019-10-29 09:48:47 -0500

    DYNAMIC ROUTE

--- bootstrap-vuejs/src/App.vue
+++ bootstrap-vuejs/src/App.vue
@@ -1,25 +1,42 @@
 <template>
   <div id="app">
-    <!-- NAVBAR -->
-    <b-navbar toggleable="md" type="dark" variant="info">
-      <!-- TITLE NAVBAR -->
-      <b-navbar-brand :to="{ name: 'home' }" class="">NavBar</b-navbar-brand>
-      <!-- TOGGLE -->
-      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-      <!-- ITEMS NAVBAR -->
-      <b-collapse id="nav-collapse" is-nav>
-        <b-navbar-nav>
-          <b-nav-item :to="{ name: 'home' }">Home</b-nav-item>
-          <b-nav-item :to="{ name: 'about' }">About</b-nav-item>
-          <b-nav-item :to="{ name: 'grid' }">Grid</b-nav-item>
-          <b-nav-item :to="{ name: 'card' }">Card</b-nav-item>
-        </b-navbar-nav>
-      </b-collapse>
-    </b-navbar>
-
+    <Navbar :menu="arrayMenu" />
     <!-- ROUTER VIEW -->
     <b-container>
       <router-view />
     </b-container>
   </div>
 </template>
+
+<script>
+import Navbar from "@/components/Navbar.vue";
+
+export default {
+  name: "App",
+  components: {
+    Navbar
+  },
+  data() {
+    return {
+      arrayMenu: [
+        {
+          name: "Home",
+          route: "/"
+        },
+        {
+          name: "About",
+          route: "about"
+        },
+        {
+          name: "Grid",
+          route: "grid"
+        },
+        {
+          name: "Card",
+          route: "card"
+        }
+      ]
+    };
+  }
+};
+</script>
---
+++ bootstrap-vuejs/src/components/Navbar.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <!-- NAVBAR -->
+    <b-navbar toggleable="md" type="dark" variant="info">
+      <!-- TITLE NAVBAR -->
+      <b-navbar-brand :to="{ name: 'home' }" class>NavBar</b-navbar-brand>
+      <!-- TOGGLE -->
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <!-- ITEMS NAVBAR -->
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav v-for="(item, index) of menu" :key="index">
+          <b-nav-item :to="item.route">{{ item.name }}</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+  props: {
+    menu: Array
+  }
+};
+</script>
