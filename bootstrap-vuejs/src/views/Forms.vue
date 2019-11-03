cd3e64fa5f1a49439a6588214b426b97618c2833
rperaza <roimerj63@gmail.com>
2019-11-02 22:09:21 -0500

    FORMS BOOTSTRAP

--- bootstrap-vuejs/src/views/Forms.vue
+++ bootstrap-vuejs/src/views/Forms.vue
@@ -1,16 +1,58 @@
 <template>
   <div class="mt-5">
     <b-row>
-      <b-col cols="12" md="10">
+      <b-col cols="12" md="10" class="d-flex justify-content-center">
         <h4>FORMULARIOS</h4>
       </b-col>
+    </b-row>
 
-      <b-col cols="12">
-        <span>{{ texto }}</span>
+    <b-row class="mt-5">
+      <!-- INPUT -->
+      <b-col cols="12" md="4" lg="3">
+        <b-row>
+          <b-col cols="12">
+            <span>{{ texto }}</span>
+          </b-col>
+
+          <b-col cols="12">
+            <b-form-input v-model="texto" type="text" :state="validateInput" />
+          </b-col>
+        </b-row>
       </b-col>
 
-      <b-col cols="12">
-        <b-form-input type="text" v-model="texto" :state="validateInput"></b-form-input>
+      <!-- SELECT -->
+      <b-col cols="12" md="4" lg="3">
+        <div v-if="animalSelected">
+          Option Selected:
+          <strong>{{ animalSelected }}</strong>
+        </div>
+        <b-form-select v-model="animalSelected" :options="animales" size="md" />
+      </b-col>
+
+      <!-- RADIO -->
+      <b-col cols="12" md="4" lg="3">
+        <div v-if="radioSelected">
+          Option Selected:
+          <strong>{{ radioSelected }}</strong>
+        </div>
+        <b-form-radio-group
+          v-model="radioSelected"
+          :options="animalesRadio"
+        ></b-form-radio-group>
+      </b-col>
+    </b-row>
+
+    <b-row class="mt-5">
+      <!-- CHECKBOX -->
+      <b-col cols="12" md="4" lg="3">
+        <div v-if="checkboxSelected.length > 0">
+          Option Selected:
+          <strong>{{ checkboxSelected }}</strong>
+        </div>
+        <b-form-checkbox-group
+          v-model="checkboxSelected"
+          :options="animalesRadio"
+        ></b-form-checkbox-group>
       </b-col>
     </b-row>
   </div>
@@ -18,10 +60,59 @@
 
 <script>
 export default {
-  name: "forms",
+  name: "Forms",
   data() {
     return {
-      texto: ""
+      texto: "",
+      animalSelected: null,
+      animales: [
+        {
+          text: "Seleccione un animal",
+          value: null
+        },
+        {
+          text: "Guau guau",
+          value: "perro"
+        },
+        {
+          text: "Miau miau",
+          value: "gato"
+        },
+        {
+          text: "Cuack",
+          value: "pato  "
+        }
+      ],
+      radioSelected: null,
+      animalesRadio: [
+        {
+          text: "Guau guau",
+          value: "perro"
+        },
+        {
+          text: "Miau miau",
+          value: "gato"
+        },
+        {
+          text: "Cuack",
+          value: "pato  "
+        }
+      ],
+      checkboxSelected: [],
+      animalesCheckbox: [
+        {
+          text: "Guau guau",
+          value: "perro"
+        },
+        {
+          text: "Miau miau",
+          value: "gato"
+        },
+        {
+          text: "Cuack",
+          value: "pato  "
+        }
+      ]
     };
   },
   computed: {
