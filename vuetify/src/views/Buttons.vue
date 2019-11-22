e0496f9823fe6c46183f261beeae855ec5b7ac62
rperaza <roimerj63@gmail.com>
2019-11-22 16:58:04 -0500

    CURSO VUE

---
+++ vuetify/src/views/Buttons.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-container>
+    <v-layout>
+      <v-flex>
+        <v-btn color="primary">primary</v-btn>&nbsp;&nbsp;
+        <v-btn color="secondary">secondary</v-btn>&nbsp;&nbsp;
+        <v-btn color="accent">accent</v-btn>&nbsp;&nbsp;
+        <v-btn color="error">error</v-btn>&nbsp;&nbsp;
+        <v-btn color="info">info</v-btn>&nbsp;&nbsp;
+        <v-btn color="success">success</v-btn>&nbsp;&nbsp;
+        <v-btn color="warning">warning</v-btn>&nbsp;&nbsp;
+        <v-btn dark color="blue">Blue dark</v-btn>&nbsp;&nbsp;
+        <v-btn color="secondary" text>Sin fondo</v-btn>&nbsp;&nbsp;
+        <v-btn color="secondary" large>Largo</v-btn>&nbsp;&nbsp;
+        <v-btn color="secondary" small>Small</v-btn>&nbsp;&nbsp;
+        <v-btn color="secondary" :disabled="disabledBtn">Disabled</v-btn>&nbsp;&nbsp;
+        <v-btn color="secondary" depressed>depressed</v-btn>&nbsp;&nbsp;
+        <v-btn text>
+          <v-icon>account_circle</v-icon>
+        </v-btn>&nbsp;&nbsp;
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      disabledBtn: true
+    };
+  }
+};
+</script>
---
+++ vuetify/src/views/Home.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-container grid-list-md>
+    <v-layout row wrap>
+      <v-flex xs12>
+        <v-card dark color="error">
+          <v-card-text class="px-0">SEIS</v-card-text>
+        </v-card>
+      </v-flex>
+
+      <v-flex xs6>
+        <v-card dark color="primary">
+          <v-card-text class="px-0">SEIS</v-card-text>
+        </v-card>
+      </v-flex>
+
+      <v-flex xs6>
+        <v-card dark color="secondary">
+          <v-card-text class="px-0">SEIS</v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
+
+    <v-layout secondary mt-5 justify-center row align-center style="height: 300px;">
+      <v-flex xs6 pink white--text>
+        <v-card dark color="info">
+          <v-card-text class="px-0">DOCE MT-5</v-card-text>
+        </v-card>
+      </v-flex>
+
+      <v-flex xs6 green white--text>
+        <v-card dark color="info">
+          <v-card-text class="px-0">DOCE MT-5</v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
+
+    <v-layout>
+      <v-flex>
+        <h1 class="display-1 text-uppercase text-right primary pa-5 mt-3">Lorem ipsum dolor.</h1>
+        <h1 class="display-2">Lorem ipsum dolor.</h1>
+        <h1 class="display-3 text-center">Lorem ipsum dolor.</h1>
+        <h1 class="display-4">Lorem ipsum dolor.</h1>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
